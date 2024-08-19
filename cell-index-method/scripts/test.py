39627ed096840f148c7f5fc24fa115122ae6be62
Juan Ramiro Castro <juancastro@itba.edu.ar>
2024-08-18 21:59:47 -0300

    benchmark various runs and generate a plot

--- cell-index-method/scripts/test.py
+++ cell-index-method/scripts/test.py
@@ -1,6 +1,8 @@
 import os
 import re
 import subprocess
+import numpy as np
+import plotly.graph_objects as go
 
 def get_output_state(output_file):
     state = {}
@@ -15,6 +17,7 @@ def get_output_state(output_file):
 
     return state
 
+
 def run_simulation(iters, m, rc, static, dynamic, periodic):
     cmd = f"java -classpath ../target/classes ar.edu.itba.ss.App -static {static} -dynamic {dynamic} -output ./tmp.txt "
     if m:
@@ -28,7 +31,9 @@ def run_simulation(iters, m, rc, static, dynamic, periodic):
 
     outputs = []
     for i in range(iters):
-        subprocess.run(cmd.split())
+        proc = subprocess.run(cmd.split())
+        if proc.returncode != 0:
+            return None
         outputs.append(get_output_state("./tmp.txt"))
 
     os.remove("./tmp.txt")
@@ -37,12 +42,33 @@ def run_simulation(iters, m, rc, static, dynamic, periodic):
 
 
 if __name__ == '__main__':
-    outputs = run_simulation(10, 3, 0.1, '../src/main/resources/Static100.txt', '../src/main/resources/Dynamic100.txt', True)
 
-    total_time = 0
-    for output in outputs:
-        total_time += output['time']
+    results = {}
+    for i in range(1, 100):
+        outputs = run_simulation(15, i, 1, '../static.txt', '../dynamic.txt', True)
+        if outputs is None:
+            # We found an M value that didn't work
+            break
+        results[i] = [output['time'] for output in outputs]
 
-    total_time /= len(outputs)
-    print("Avg time: ", total_time)
+    means = [np.mean(values) for values in results.values()]
+    std_devs = [np.std(values) for values in results.values()]
+    categories = list(results.keys())
 
+    # Create bar chart with error bars
+    fig = go.Figure(data=[
+        go.Bar(
+            x=categories,
+            y=means,
+            error_y=dict(type='data', array=std_devs, visible=True)
+        )
+    ])
+
+    fig.update_layout(
+        title='Time to Execute',
+        xaxis_title='M Value',
+        yaxis_title='Time (ms)',
+        xaxis=dict(tickmode='linear')
+    )
+
+    fig.show()
