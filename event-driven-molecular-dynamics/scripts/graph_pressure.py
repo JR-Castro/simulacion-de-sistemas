fa5cceb431268053fbca257c55b32bc374a3ca7e
Juan Ramiro Castro <juancastro@itba.edu.ar>
2024-09-18 22:31:23 -0300

    pressure calculation done?

--- event-driven-molecular-dynamics/scripts/graph_pressure.py
+++ event-driven-molecular-dynamics/scripts/graph_pressure.py
@@ -50,7 +50,7 @@ def calculate_obstacle_pressures(static, collisions, dt):
 
         output.append({
             'time': s_time,
-            'pressure': momentum / (dt * 2 * math.pi * obstacle_radius)
+            'pressure': momentum / (len(filtered_collisions) * dt * 2 * math.pi * obstacle_radius)
         })
 
     print(f"Minimum collisions: {minimum_collisions}")
@@ -85,28 +85,40 @@ def calculate_wall_pressures(static, collisions, dt):
 
         output.append({
             'time': s_time,
-            'pressure': momentum / (dt * 2 * math.pi * radius)
+            'pressure': momentum / (len(filtered_collisions) * dt * 2 * math.pi * radius)
         })
 
     print(f"Minimum collisions: {minimum_collisions}")
     return output
 
 
-def graph_pressure(pressures, output_file):
-    times = [p['time'] for p in pressures[0]]
-    data = [[p['pressure'] for p in pressure] for pressure in pressures]
+def graph_pressure(wall_pressures, obstacle_pressures, output_file):
+    wall_times = [p['time'] for p in wall_pressures[0]]
+    wall_data = [[p['pressure'] for p in pressure] for pressure in wall_pressures]
 
-    np_data = np.array(data)
-    avg = np.mean(np_data, axis=0)
-    std = np.std(np_data, axis=0)
+    obstacle_times = [p['time'] for p in obstacle_pressures[0]]
+    obstacle_data = [[p['pressure'] for p in pressure] for pressure in obstacle_pressures]
+
+    wall_np = np.array(wall_data)
+    wall_mean = np.mean(wall_np, axis=0)
+    wall_std = np.std(wall_np, axis=0)
+
+    obstacle_np = np.array(obstacle_data)
+    obstacle_mean = np.mean(obstacle_np, axis=0)
+    obstacle_std = np.std(obstacle_np, axis=0)
 
     # Improve the y-axis scale by narrowing the limits to better visualize variations
-    plt.plot(times, avg, linestyle='--', marker='o', color='blue')
+    plt.plot(wall_times, wall_mean, linestyle='--', marker='o', color='blue', label='Pared')
+    plt.plot(obstacle_times, obstacle_mean, linestyle='--', marker='o', color='red', label='Obstáculo')
 
-    plt.fill_between(times,
-                     [a - s for a, s in zip(avg, std)],
-                     [a + s for a, s in zip(avg, std)],
+    plt.fill_between(wall_times,
+                     [a - s for a, s in zip(wall_mean, wall_std)],
+                     [a + s for a, s in zip(wall_mean, wall_std)],
                      color='blue', alpha=0.2)
+    plt.fill_between(obstacle_times,
+                     [a - s for a, s in zip(obstacle_mean, obstacle_std)],
+                     [a + s for a, s in zip(obstacle_mean, obstacle_std)],
+                     color='red', alpha=0.2)
 
     plt.xlabel("Tiempo (s)", fontsize=12)
     plt.ylabel("Presión (N/m)", fontsize=12)
@@ -116,7 +128,8 @@ def graph_pressure(pressures, output_file):
 
     plt.grid(True, linestyle='--', alpha=0.6)
     # plt.title('Pressure over Time', fontsize=14)
-    # plt.legend(loc='upper right')
+    # Legend outside of graph
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), shadow=True)
     plt.tight_layout()
 
     plt.savefig(output_file, dpi=400)
@@ -145,11 +158,10 @@ if __name__ == '__main__':
 
     collisions = [read_collisions(path.join(output_path, f)) for f in files]
 
-    wall_pressures = [calculate_wall_pressures(static, c, 0.1) for c in collisions]
+    wall_pressures = [calculate_wall_pressures(static, c, 0.01) for c in collisions]
     print()
     obstacle_pressures = [calculate_obstacle_pressures(static, c, 0.1) for c in collisions]
 
-    graph_pressure(wall_pressures, "wall_pressure.png")
-    graph_pressure(obstacle_pressures, "obstacle_pressure.png")
+    graph_pressure(wall_pressures, obstacle_pressures, "pressures.png")
 
-    print(f"Total time: {time.time() - start_time}s")
+    print(f"\nTotal time: {time.time() - start_time}s")
