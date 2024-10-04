996a23982e6888c1dc0c442f6c8caef5ea681ab9
Juan Ramiro Castro <juancastro@itba.edu.ar>
2024-10-04 11:49:18 -0300

    calculate amplitudes

---
+++ molecular-dynamics/scripts/amplitude_analysis.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+N = 100
+FONT = {'family': 'serif',
+        'color': 'black',
+        'weight': 'normal',
+        'size': 14}
+
+if __name__ == '__main__':
+    runs = range(0, 9)
+    files = [f'output/coupled_oscillator_w_{i}.csv' for i in runs]
+
+    wValues = [9.0 + i * 0.25 for i in runs]
+    amplitude = []
+    i = 0
+    for file in files:
+        df = pd.read_csv(file)
+
+        # frames = int(len(df['time'])/N)
+        # amplitudes = [df.iloc[i * N:(i+1)*N,:]['position'].max() for i in range(frames)]
+        #
+        # plt.plot(range(frames), amplitudes)
+        # plt.xlabel('Frame', fontdict=FONT)
+        # plt.ylabel('Amplitude (m)', fontdict=FONT)
+        # plt.savefig(f'amplitude_analysis_{i}.png', dpi=300)
+        # plt.clf()
+        # i+=1
+
+        amplitude.append(df['position'].max())
+
+    plt.plot(wValues, amplitude, 'o-')
+
+
+    plt.xlabel('w (rad/s)', fontdict=FONT)
+    plt.ylabel('Amplitude (m)', fontdict=FONT)
+
+
+    plt.savefig('amplitude_analysis.png', dpi=300)
+
--- molecular-dynamics/src/main/kotlin/CoupledOscillatorsMain.kt
+++ molecular-dynamics/src/main/kotlin/CoupledOscillatorsMain.kt
@@ -1,5 +1,7 @@
 package ar.edu.itba.ss
 
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.io.File
@@ -9,20 +11,21 @@ import kotlin.math.pow
 fun main() = runBlocking {
     val startTime = System.currentTimeMillis()
 
-    val maxTime = 80.0
+    val maxTime = 120.0
     val k = 100
     val amplitude = 10.0.pow(-2)
 //    val l0 = 10.0.pow(-3)
     val N = 100
-    val wValues = listOf(5.0, 7.5, 10.0, 12.5, 15.0)
+    val wValues = (0..8).map { 9.0 + it * 0.25 }
     val dtValues = wValues.map { min(1E-3, 1 / (100 * it)) }
     val dt2 = 0.05
     val mass = 0.001
 
+    val jobs = mutableListOf<Job>()
 
     for (i in wValues.indices) {
         val outputFile = File("output/coupled_oscillator_w_$i.csv")
-        launch {
+        jobs.add(launch {
             CoupledOscillator(
                 maxTime,
                 k.toDouble(),
@@ -34,8 +37,9 @@ fun main() = runBlocking {
                 mass,
                 outputFile
             ).run()
-        }
+        })
     }
 
+    jobs.joinAll()
     println("Execution time: ${System.currentTimeMillis() - startTime}ms")
 }
